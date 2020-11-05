ec862e849da908c83cceeb96fc40cc155e175396
Wataru Ono <wtr0414@gmail.com>
2020-11-05 16:02:07 +0900

    :sparkles: 最低限の機能を実装

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,47 @@
+<div>
+	絞り込み:
+	<button on:click={() => { condition = null }}>すべて</button>
+	<button on:click={() => { condition = false }}>未完了</button>
+	<button on:click={() => { condition = true }}>完了</button>
+</div>
+<div>
+	<input type="text" bind:value={title}>
+	<button on:click={() => add()}>タスク追加</button>
+</div>
+<div>
+	<ul>
+		{#each filterdTodoList(todoList, condition) as todo (todo.id)}
+			<li>
+				<input type="checkbox" bind:checked={todo.done}> {todo.title}
+			</li>
+		{/each}
+	</ul>
+</div>
+
 <script>
-	export let name;
+	let todoList = [
+		{ id: 0, done: false, title: 'レストランを予約する' },
+		{ id: 1, done: false, title: 'サプライズ用の指輪を買う' },
+		{ id: 2, done: false, title: 'フラッシュモブダンスを練習する' },
+	]
+
+	let title = ''
+	function add() {
+		todoList = [...todoList,
+			{
+				id: todoList.length,
+				done: false,
+				title: title
+			}]
+	}
+
+	let condition = null
+
+	$: filterdTodoList = (todoList, condition) => {
+		return condition === null ? todoList : todoList.filter(t => t.done === condition)
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
